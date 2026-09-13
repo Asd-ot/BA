2ecdec2afa086cd62c7058344b44c750eb3985a3
Asd-ot <akhildhawale62898@gmail.com>
2026-09-13 11:50:07 +0530

    Add initial implementation of bright_mart.py

---
+++ bright_mart.py
@@ -0,0 +1,5 @@
+€•)      Œsklearn.linear_model._base”ŒLinearRegression”“”)”}”(Œ
+fit_intercept”ˆŒcopy_X”ˆŒn_jobs”NŒpositive”‰Œfeature_names_in_”Œnumpy._core.multiarray”Œ_reconstruct”“”Œnumpy”Œ ndarray”“”K …”Cb”‡”R”(KK…”h
+Œdtype”“”ŒO8”‰ˆ‡”R”(KŒ|”NNNJÿÿÿÿJÿÿÿÿK?t”b‰]”(ŒTV”ŒRadio”Œ	Newspaper”et”bŒn_features_in_”KŒcoef_”hhK …”h‡”R”(KK…”hŒf8”‰ˆ‡”R”(KŒ<”NNNJÿÿÿÿJÿÿÿÿK t”b‰CžScÉæ¦?74@%‹7È?@y~ùyžf?”t”bŒrank_”KŒ	singular_”hhK …”h‡”R”(KK…”h)‰Cä‡ˆ,¢@ìàÔ®ãq@<ˆõy´0d@”t”bŒ
+intercept_”h
+Œscalar”“”h)C8Œ®A!Õ @”†”R”Œ_sklearn_version”Œ1.6.1”ub. 
