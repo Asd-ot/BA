5c7ba2c77211a1662a93bb6db137cd37c0af7596
Asd-ot <akhildhawale62898@gmail.com>
2026-09-13 12:04:52 +0530

    Update bright_mart.py

--- bright_mart.py
+++ bright_mart.py
@@ -1,5 +1,21 @@
-€•)      Œsklearn.linear_model._base”ŒLinearRegression”“”)”}”(Œ
-fit_intercept”ˆŒcopy_X”ˆŒn_jobs”NŒpositive”‰Œfeature_names_in_”Œnumpy._core.multiarray”Œ_reconstruct”“”Œnumpy”Œ ndarray”“”K …”Cb”‡”R”(KK…”h
-Œdtype”“”ŒO8”‰ˆ‡”R”(KŒ|”NNNJÿÿÿÿJÿÿÿÿK?t”b‰]”(ŒTV”ŒRadio”Œ	Newspaper”et”bŒn_features_in_”KŒcoef_”hhK …”h‡”R”(KK…”hŒf8”‰ˆ‡”R”(KŒ<”NNNJÿÿÿÿJÿÿÿÿK t”b‰CžScÉæ¦?74@%‹7È?@y~ùyžf?”t”bŒrank_”KŒ	singular_”hhK …”h‡”R”(KK…”h)‰Cä‡ˆ,¢@ìàÔ®ãq@<ˆõy´0d@”t”bŒ
-intercept_”h
-Œscalar”“”h)C8Œ®A!Õ @”†”R”Œ_sklearn_version”Œ1.6.1”ub. 
+import streamlit as st
+import joblib
+import numpy as np
+
+# Load the saved model
+model = joblib.load(open('linear_reg.sav', 'rb'))
+
+st.title('Sales Prediction App')
+
+# Input features
+TV = st.number_input('TV Advertising Budget', min_value=0.0)
+Radio = st.number_input('Radio Advertising Budget', min_value=0.0)
+Newspaper = st.number_input('Newspaper Advertising Budget', min_value=0.0)
+
+# Make prediction
+if st.button('Predict Sales'):
+    input_data = np.array([[TV, Radio, Newspaper]])
+    prediction = model.predict(input_data)[0]
+    st.success(f'Predicted Sales: {prediction:.2f}')
+
+ 
